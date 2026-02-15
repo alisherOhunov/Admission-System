10c8e4977ec24509f4dbfbca22bb764baa53b624
Alisher <alisheroxunov1383@gmail.com>
2026-02-15 09:28:57 +0500

    update start term selection to use current period for dynamic options

--- resources/views/applicant/partials/program-choice.blade.php
+++ resources/views/applicant/partials/program-choice.blade.php
@@ -75,7 +75,7 @@
                                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                                     <option value="">{{ __('applicant/program-choice.select_start_term') }}
                                     </option>
-                                    <option value="fall2025" @selected(old('start_term', $application->start_term ?? '') == 'fall2025')>Fall 2025</option>
+                                    <option value="{{ $currentPeriod->id }}" @selected(old('start_term', $application->start_term ?? '') == $currentPeriod->id)>{{ $currentPeriod->name }}</option>
                                 </select>
                                 <p class="mt-2 text-gray-500">
                                     {{ __('applicant/program-choice.start_term_placeholder') }}</p>
