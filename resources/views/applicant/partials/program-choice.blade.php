e7d43d8a78f48c1f72fb264342a1a0db6fb861b1
Alisher <alisheroxunov1383@gmail.com>
2026-02-16 08:43:12 +0500

    fix: update start term selection to use period name instead of ID

--- resources/views/applicant/partials/program-choice.blade.php
+++ resources/views/applicant/partials/program-choice.blade.php
@@ -75,7 +75,7 @@
                                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                                     <option value="">{{ __('applicant/program-choice.select_start_term') }}
                                     </option>
-                                    <option value="{{ $currentPeriod->id }}" @selected(old('start_term', $application->start_term ?? '') == $currentPeriod->id)>{{ $currentPeriod->name }}</option>
+                                    <option value="{{ $currentPeriod->name }}" @selected(old('start_term', $application->start_term ?? '') == $currentPeriod->name)>{{ $currentPeriod->name }}</option>
                                 </select>
                                 <p class="mt-2 text-gray-500">
                                     {{ __('applicant/program-choice.start_term_placeholder') }}</p>
